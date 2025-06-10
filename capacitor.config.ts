01de8fcca73db0e38c14443b5142d0fd4002da69
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-06-10 13:03:34 -0500

    update to the latest capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.ionicweatherreact',
   appName: 'ionic-weather-react',
   webDir: 'dist',
+  android: {
+    adjustMarginsForEdgeToEdge: 'auto',
+  },
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+    },
+  },
 };
 
 export default config;
